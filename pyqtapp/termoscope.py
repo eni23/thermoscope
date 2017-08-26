baf8b1c967683116d07c5f10195a845982e95f28
Cyrill von Wattenwyl <eni@e23.ch>
2017-08-26 21:40:11 +0200

    implement resolution and more fancy stuff

--- pyqtapp/termoscope.py
+++ pyqtapp/termoscope.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QTime, QTimer
+from PyQt5.QtCore import QTime, QTimer, QThread, pyqtSignal
 
 from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
@@ -17,29 +17,20 @@ import array
 import copy
 
 
-import pytz
-UNIX_EPOCH_naive = datetime.datetime(1970, 1, 1, 0, 0) #offset-naive datetime
-UNIX_EPOCH_offset_aware = datetime.datetime(1970, 1, 1, 0, 0, tzinfo = pytz.utc) #offset-aware datetime
-UNIX_EPOCH = UNIX_EPOCH_naive
 
+inital_sensor_resolution = 9
+
+
+UNIX_EPOCH_naive = datetime.datetime(1970, 1, 1, 0, 0) #offset-naive datetime
+UNIX_EPOCH = UNIX_EPOCH_naive
 TS_MULT_us = 1e6
 
-
 def now_timestamp(ts_mult=TS_MULT_us, epoch=UNIX_EPOCH):
-    return(int((datetime.datetime.utcnow() - epoch).total_seconds()*ts_mult))
+    return(int((datetime.datetime.now() - epoch).total_seconds()*ts_mult))
 
 def int2dt(ts, ts_mult=TS_MULT_us):
     return(datetime.datetime.utcfromtimestamp(float(ts)/ts_mult))
 
-def dt2int(dt, ts_mult=TS_MULT_us, epoch=UNIX_EPOCH):
-    delta = dt - epoch
-    return(int(delta.total_seconds()*ts_mult))
-
-def td2int(td, ts_mult=TS_MULT_us):
-    return(int(td.total_seconds()*ts_mult))
-
-def int2td(ts, ts_mult=TS_MULT_us):
-    return(datetime.timedelta(seconds=float(ts)/ts_mult))
 
 
 class TimeAxisItem(pg.AxisItem):
@@ -56,9 +47,14 @@ param_tpl = {
     'name': 'Sensor', 'type': 'group', 'children': [
         {'name': 'Show Plot', 'type': 'bool', 'value': True },
         {'name': 'Show Dots', 'type': 'bool', 'value': False },
-        #{'name': 'Resolution', 'type': 'list', 'values': {"9": 9, "10": 10, "11": 11, "12":12 }, 'value': 12},
+        {'name': 'Resolution', 'type': 'list', 'values': {
+            "9": 9,
+            "10": 10,
+            "11": 11,
+            "12": 12
+            }, 'value': inital_sensor_resolution },
         {'name': 'Line width', 'type': 'int', 'value': 1 },
-        # insert col
+        # insert color line dynamically
     ]}
 
 
@@ -66,35 +62,60 @@ def colortupletohex(color_tuple):
     a = array.array('B',color_tuple )
     return a.tobytes().hex()
 
+
+class SerialReaderThread(QThread):
+    newValue = pyqtSignal(int, float)
+
+    def __init__(self):
+        QThread.__init__(self)
+
+    def __del__(self):
+        self.wait()
+
+    def set_serial(self, ser):
+        self.serial = ser
+
+    def run(self):
+        while True:
+            data_raw = self.serial.read(3)
+            sensor_id, temp_raw = struct.unpack("=BH", data_raw)
+            temp = temp_raw / 100
+            self.newValue.emit(sensor_id, temp)
+
+
 class ThermoscopeApp(QtGui.QApplication):
 
 
     def __init__(self, *args, **kwargs):
         super(ThermoscopeApp, self).__init__(*args, **kwargs)
-        self.t = QTime()
-        self.t.start()
+
         self.init_serial()
+        self.ser_thread = SerialReaderThread()
+        self.ser_thread.set_serial(self.serial)
+        self.ser_thread.newValue.connect(self.update_temp)
+
+        self.win = pg.GraphicsWindow(title="Thermoscope")
+        self.win.resize(1000,600)
+
+        # set inital_sensor_resolution
+        for i in range(0,4):
+            self.set_sens_resolution(i+1, inital_sensor_resolution)
 
         self.data = []
-        self.dataidx = []
         self.curve_colors = [
             (255,0,0),
             (0,255,0),
             (0,0,255),
             (255,255,0)
         ]
+        self.line_width = [ 1, 1, 1, 1 ]
 
-        maxlen = 200
         for i in range(0,4):
             self.data.append([
                 deque(), #x
                 deque()  #y
             ])
-            self.dataidx.append({})
-            maxlen = 200
 
-        self.win = pg.GraphicsWindow(title="Thermoscope")
-        self.win.resize(1000,600)
 
         # param tree
         self.params_data = []
@@ -115,10 +136,10 @@ class ThermoscopeApp(QtGui.QApplication):
 
         self.params.sigTreeStateChanged.connect(self.change_tree)
 
-
-
-        self.plot = pg.PlotWidget(title='Timed data', axisItems={'bottom': TimeAxisItem(orientation='bottom')})
-        #self.plot = self.win.addPlot(title='Timed data', axisItems={'bottom': TimeAxisItem(orientation='bottom')})
+        self.plot = pg.PlotWidget(
+            title='',
+            axisItems={'bottom': TimeAxisItem(orientation='bottom')}
+        )
 
         self.layout.addWidget(self.param_tree, 0, 0, 1, 1)
         self.layout.addWidget(self.plot, 0, 1, 1, 8)
@@ -130,45 +151,21 @@ class ThermoscopeApp(QtGui.QApplication):
                 self.plot.plot(
                     pen=pg.mkPen(self.curve_colors[i], width=1),
                     name="sensor {0}".format(i),
-                    #symbolBrush=(0,0,0),
-                    #symbolPen=(255,255,255),
-                    #symbolSize=3
                 )
             )
 
+        self.ser_thread.start()
 
-
-        #cross hair
-        """self.vLine = pg.InfiniteLine(angle=90, movable=False)
-        self.hLine = pg.InfiniteLine(angle=0, movable=False)
-        self.plot.addItem(self.vLine, ignoreBounds=True)
-        self.plot.addItem(self.hLine, ignoreBounds=True)
-        self.proxy = pg.SignalProxy(self.plot.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
-        """
-
-        self.tmr = QTimer()
-        self.tmr.timeout.connect(self.update)
-        self.tmr.start(100)
-
-    def mouseMoved(self, evt):
-        pos = evt[0]  ## using signal proxy turns original arguments into a tuple
-        if self.plot.sceneBoundingRect().contains(pos):
-            mousePoint = self.plot.vb.mapSceneToView(pos)
-            index = int(mousePoint.x())
-            #if index > 0 and index < len(data1):
-            #    label.setText("<span style='font-size: 12pt'>x=%0.1f,   <span style='color: red'>y1=%0.1f</span>,   <span style='color: green'>y2=%0.1f</span>" % (mousePoint.x(), data1[index], data2[index]))
-            self.vLine.setPos(mousePoint.x())
-            self.hLine.setPos(mousePoint.y())
 
     def change_tree(self, params, changes):
         for param, change, data in changes:
             path = self.params.childPath(param)
             change = path[1]
             sensor_id = int(path[0].split(" ")[1])
-            print('  sensor: %s'% sensor_id)
-            print('  change: %s' % change)
-            print('  data:      %s'% str(data))
-            print('  ----------')
+            #print('  sensor: %s'% sensor_id)
+            #print('  change: %s' % change)
+            #print('  data:      %s'% str(data))
+            #print('  ----------')
 
             data_id = sensor_id - 1
             if change=="Line color":
@@ -177,7 +174,10 @@ class ThermoscopeApp(QtGui.QApplication):
                     color = data.name()[1:]
                 except AttributeError:
                     color = data
-                self.curves[data_id].setData(pen=color)
+                lpen = pg.mkPen(color, width=self.line_width[data_id] )
+                self.curves[data_id].setData(
+                    pen = lpen
+                )
                 self.curve_colors[data_id] = color
 
             if change=="Show Plot":
@@ -203,25 +203,26 @@ class ThermoscopeApp(QtGui.QApplication):
                 self.curves[data_id].setData(
                     pen = lpen
                 )
+                self.line_width[data_id] = int(data)
+
+            if change == "Resolution":
+                self.set_sens_resolution(sensor_id, data)
+
+
+    def set_sens_resolution(self, sensor_id, value):
+        msg = struct.pack("=BBB", 1, sensor_id, int(value) )
+        self.serial.write(msg)
+
 
     def init_serial(self):
         self.serial = serial.Serial('/dev/ttyUSB0', 115200)
 
-    def update(self):
-        data_raw = self.serial.read(3)
-        sensor_id, temp_raw = struct.unpack("=BH", data_raw)
-        temp = temp_raw / 100
+
+    def update_temp(self, sensor_id, temp):
         data_id = sensor_id-1
-
-        #print("sensor={0} temp={1}".format(sensor_id, temp))
-        #if sensor_id == 1:
-        #self.data.append({'x': self.t.elapsed(), 'y': np.random.randint(0, 100)})
-
         ts = now_timestamp()
         self.data[data_id][0].append(ts)
         self.data[data_id][1].append(temp)
-        self.dataidx[data_id][ts] = temp
-
         self.curves[data_id].setData(
             x=list(self.data[data_id][0]),
             y=list(self.data[data_id][1])
--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,18 @@ DS18B20 temp4;
 
 int temp_read_delay = 900;
 uint8_t temp_resolution = 12;
+uint8_t current_temp[4][2];
+uint8_t serial_curr_pos = 0;
+uint8_t serial_msg[4];
 
 
-void print_temp(uint8_t sens_id, float temp_c){
+void store_temp(uint8_t sens_id, float temp_c){
   uint16_t itemp = (temp_c * 100);
   uint8_t temp[2] = { itemp & 0xff, itemp >> 8 };
-  Serial.print(sens_id, DEC);
-  Serial.print("=");
-  Serial.println(temp_c);
+  current_temp[sens_id][0] = temp[0];
+  current_temp[sens_id][1] = temp[1];
+  Serial.write((int)sens_id);
+  Serial.write(temp, 2);
 }
 
 
@@ -36,22 +40,22 @@ void setup() {
 
   temp1.on_temp([](float t_new){
     temp1.read();
-    print_temp(1, t_new);
+    store_temp(1, t_new);
   });
 
   temp2.on_temp([](float t_new){
     temp2.read();
-    print_temp(2, t_new);
+    store_temp(2, t_new);
   });
 
   temp3.on_temp([](float t_new){
     temp3.read();
-    print_temp(3, t_new);
+    store_temp(3, t_new);
   });
 
   temp4.on_temp([](float t_new){
     temp4.read();
-    print_temp(4, t_new);
+    store_temp(4, t_new);
   });
 
   temp1.read();
@@ -61,8 +65,55 @@ void setup() {
 }
 
 
+void parse_serial(){
+  uint8_t cmd = serial_msg[0];
+  uint8_t sensor = serial_msg[1];
+  uint8_t data = serial_msg[2];
+
+  // change resolution
+  if ( cmd == 1 ){
+
+    if (data>12){
+      data = 12;
+    }
+    if (data<9){
+      data = 9;
+    }
+
+    if (sensor == 1){
+      temp1.set_resolution(data);
+    }
+    if (sensor == 2){
+      temp2.set_resolution(data);
+    }
+    if (sensor == 3){
+      temp3.set_resolution(data);
+    }
+    if (sensor == 4){
+      temp4.set_resolution(data);
+    }
+    
+    return;
+  }
+
+}
+
+
+
+void read_serial(){
+  serial_msg[serial_curr_pos] = Serial.read();
+  serial_curr_pos++;
+  if (serial_curr_pos>2){
+    serial_curr_pos = 0;
+    parse_serial();
+  }
+}
+
 
 void loop() {
+  if (Serial.available() > 0) {
+    read_serial();
+  }
   temp1.run();
   temp2.run();
   temp3.run();
